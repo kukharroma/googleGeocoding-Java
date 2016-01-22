74084645e4f976278ffae793cecf853ee316f750
Stafiiyevskyi <a.stafiyevsky@gmail.com>
2016-01-22 17:49:46 +0200

    Changed RestClient.

--- library/src/main/java/com/winguys/geocoding/api/RestClient.java
+++ library/src/main/java/com/winguys/geocoding/api/RestClient.java
@@ -1,12 +1,6 @@
-package com.winguys.geocoding.geocodingService;
+package com.winguys.geocoding.api;
 
-import com.squareup.okhttp.Interceptor;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
 import com.winguys.geocoding.constant.GeocodingUrl;
-
-import java.io.IOException;
 
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
